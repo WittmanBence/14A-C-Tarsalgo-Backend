d9a2dd275a44ff67ecf89e57540ba1efc4d2f88a
Varga Balázs Ádám <112722847+VargaBalazsAdam@users.noreply.github.com>
2023-03-29 10:11:23 +0200

    fixes

--- data/import_1side_to_localhost.bat
+++ data/import_1side_to_localhost.bat
@@ -1,1 +1,1 @@
-"c:\Program Files\MongoDB\Server\5.0\bin\mongoimport.exe" --uri="mongodb://127.0.0.1:27017/Tarsalgo" --collection=1side --drop --file=D:\Ilias_David\Tarsalgo-Backend\data\1side.json --jsonArray
+"c:\Program Files\MongoDB\Server\5.0\bin\mongoimport.exe" --uri="mongodb://127.0.0.1:27017/Tarsalgo" --collection=passages --drop --file=D:\Ilias_David\Tarsalgo-Backend\data\passages.json --jsonArray
